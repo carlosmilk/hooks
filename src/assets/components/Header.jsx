c9c717aee2c4b6bc89950f49f47ebc641df41c3c
Carlos Eduardo Leite de Oliveira <carlosoliveirahl13@gmail.com>
2024-10-26 16:58:29 -0300

    Inicial

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    let n1, n2, resultados;
+    n1 = 5
+    n2 = 10
+    resultados = n1 + n2
+
+    return(
+        <>
+        {resultados}
+        </>
+    )
+}
+export default Header
